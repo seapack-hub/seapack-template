c2202e79dcbd2fe44a0e255c66f0b335f13a4c09
zenghaifeng <3270937741@qq.com>
2024-04-10 20:55:18 +0800

    添加依赖布局

--- src/layout/menuTab/index.vue
+++ src/layout/menuTab/index.vue
@@ -1,16 +1,19 @@
 <template>
-  <div class="common-layout">
-    <el-container>
-      <el-header>头部</el-header>
-      <el-main>主体</el-main>
-      <el-footer>尾部</el-footer>
-    </el-container>
-  </div>
+  <!--设置全屏-->
+  <el-container>
+    <el-header>头部</el-header>
+    <el-main>主体</el-main>
+    <el-footer>尾部</el-footer>
+  </el-container>
 </template>
 
 <script setup lang="ts">
 </script>
 
 <style scoped lang="scss">
-
+.el-container{
+  width: 100vw;
+  height: 100vh;
+  background-color: #646cff;
+}
 </style>
