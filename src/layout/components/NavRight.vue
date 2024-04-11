c852aed61142cf13296a6d330cf36e0fa0a555ef
zenghaifeng <3270937741@qq.com>
2024-04-11 18:28:42 +0800

    设置全屏

--- src/layout/components/NavRight.vue
+++ src/layout/components/NavRight.vue
@@ -4,12 +4,21 @@
     <div class="setting-navigation">
       <!--消息通知-->
       <SPIcon name="message-notify" size="20px"></SPIcon>
+      <!--全屏设置-->
+      <div class="setting-item" @click="toggle">
+        <SPIcon :name="isFullscreen?'fullscreen-shrink':'fullscreen-expand'" size="20px"></SPIcon>
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
+import {useFullscreen} from '@vueuse/core';
 
+/**
+ * VueUse全屏
+ */
+const {isFullscreen,toggle} = useFullscreen()
 </script>
 
 <style scoped lang="scss">
@@ -17,5 +26,9 @@
   display: flex;
   flex-direction: row;
   align-items: center;
+  .setting-navigation{
+    display: flex;
+    gap: 10px;
+  }
 }
 </style>
