5762ad56d8b330c00aa19aec6d21afd4f2f8578c
曾海峰 <zenghf@tsingyun.net>
2024-07-26 17:59:09 +0800

    全局设置功能开发

---
+++ src/layout/main/LeftMode.vue
@@ -0,0 +1,35 @@
+<template>
+  <el-container style="width: 100vw;height:100vh">
+    <el-aside>
+      <SideBar/>
+    </el-aside>
+    <el-container>
+      <el-header>
+        <NavigationBar/>
+      </el-header>
+      <el-main>
+        <router-view/>
+      </el-main>
+    </el-container>
+  </el-container>
+</template>
+
+<script setup lang="ts">
+import {NavigationBar, SideBar} from "@/layout/components";
+import {useAppStore} from "@/store/modules/app.ts";
+import {computed} from "vue";
+
+const appStore = useAppStore();
+const sidebarWidth = computed(()=>{
+  return appStore.opened?"var(--sidebar-width)":"var(--sidebar-hide-width)"
+})
+</script>
+
+<style scoped lang="scss">
+.el-aside{
+  width: v-bind(sidebarWidth);
+  //设置平滑过渡效果，监听宽度变化，平滑5秒
+  transition-property: width;
+  transition-duration: 0.5s;
+}
+</style>
--- src/layout/main/index.vue
+++ src/layout/main/index.vue
@@ -1,17 +1,5 @@
 <template>
-  <el-container style="width: 100vw;height:100vh">
-    <el-aside>
-      <SideBar/>
-    </el-aside>
-    <el-container>
-      <el-header>
-        <NavigationBar/>
-      </el-header>
-      <el-main>
-        <router-view/>
-      </el-main>
-    </el-container>
-  </el-container>
+  <LeftMode/>
   <!--系统配置-->
   <right-panel>
     <Settings/>
@@ -20,21 +8,9 @@
 
 <script setup lang="ts">
 import RightPanel from "@/layout/rightPanel/index.vue"
-import {NavigationBar,SideBar,Settings} from "@/layout/components/index.ts"
-import {useAppStore} from "@/store/modules/app.ts";
-import {computed} from "vue";
-
-const appStore = useAppStore();
-const sidebarWidth = computed(()=>{
-  return appStore.opened?"var(--sidebar-width)":"var(--sidebar-hide-width)"
-})
+import {Settings} from "@/layout/components/index.ts"
+import LeftMode from "./LeftMode.vue"
 </script>
 
 <style scoped lang="scss">
-.el-aside{
-  width: v-bind(sidebarWidth);
-  //设置平滑过渡效果，监听宽度变化，平滑5秒
-  transition-property: width;
-  transition-duration: 0.5s;
-}
 </style>
