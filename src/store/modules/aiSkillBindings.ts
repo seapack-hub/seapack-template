765ec07f6afad3163cbf5be6b756bd485bf8ac7b
曾海峰 <7753230+zenghaifenga@user.noreply.gitee.com>
2026-07-01 18:12:53 +0800

    注释

--- src/store/modules/aiSkillBindings.ts
+++ src/store/modules/aiSkillBindings.ts
@@ -49,6 +49,9 @@ export const useAiSkillBindingsStore = defineStore('aiSkillBindings', () => {
     loading.value = true
     try {
       const res = await SkillAPI.getAllBindings()
+
+      //对应值  元素值 params 中的 options 
+      //options: "[{\"label\": \"技术教程\", \"value\": \"技术教程\"}, {\"label\": \"经验分享\", \"value\": \"经验分享\"}, {\"label\": \"行业分析\", \"value\": \"行业分析\"}, {\"label\": \"产品介绍\", \"value\": \"产品介绍\"}]"
       bindings.value = res || []
       loaded.value = true
     } finally {
